0961e305a4139691bfe53a89e7e5839dac0c105f
Katharina Hammerschmidt <katharinahammerschmidt@Katharinas-MacBook-Air.fritz.box>
2022-02-20 23:17:21 +0100

    changes for pull request

--- src/cmd_redirections.c
+++ src/cmd_redirections.c
@@ -4,6 +4,14 @@
 //redirections for pipes
 //die letzte redirection zählt
 
+// '>>'
+// Appending Redirected Output
+// Redirection of output in this fashion causes the file whose name results from the expansion of word to be opened for appending on file descriptor n,
+// or the standard output (file descriptor 1) if n is not specified. If the file does not exist it is created.
+// The general format for appending output is:
+// [n]>>word
+
+
 // '<<'
 //3.6.6 Here Documents
 // This type of redirection instructs the shell to read input from the current source until a line containing only word (with no trailing blanks) i
@@ -20,17 +28,65 @@
 // here-documents within shell scripts to be indented in a natural fashion.
 
 
-
-
-/* Cuts out the redirections associated with the outfile from the cmd_line. */
-void	cut_outfile_red(char **string)
+/* Cuts out the redirections associated with the infile from the cmd_line. */
+void	ft_cut_infile_redirections(char **string)
 {
 	char	*tmp;
 	int		i;
-	int		p;
+	// int		p;
 
 	i = 0;
-	p = 0;
+	// p = 0;
+	tmp = NULL;
+	while ((*string)[i] != '<')
+	{
+		tmp = ft_strnjoin(tmp, (*string)[i], 1);
+		i++;
+	}
+	while ((*string)[i] == '<')
+		i++;
+	while ((*string)[i] == ' ' && (*string)[i] != '\0')
+		i++;
+	i++;
+	while ((*string)[i] != ' ' && (*string)[i] != '\0')
+		i++;
+	while ((*string)[i] == ' ' && (*string)[i] != '\0')
+		i++;
+	while ((*string)[i] != '\0')
+		tmp = ft_strnjoin(tmp, (*string)[i++], 1);
+	free(*string);
+	*string = NULL;
+	*string = tmp;
+}
+
+/* Saves the infile in the t_info struct. */
+void	infile_redirection(char **string, t_vars *ms)
+{
+	int		k;
+	int		j;
+	char	*tmp;
+
+	k = 0;
+	j = ft_strchr_pos(*string, '<');
+	while (((*string)[j] == '<' || (*string)[j] == ' ') && (*string)[j] != '\0')
+		j++;
+	k = j;
+	while ((*string)[k] != ' ' && (*string)[k] != '\0')
+		k++;
+	tmp = ft_substr(*string, j, k - j);
+	ms->info->infile = tmp;
+	ft_cut_infile_redirections(string);
+}
+
+/* Cuts out the redirections associated with the outfile from the cmd_line. */
+void	ft_cut_outfile_redirections(char **string)
+{
+	char	*tmp;
+	int		i;
+	// int		p;
+
+	i = 0;
+	// p = 0;
 	tmp = NULL;
 	while ((*string)[i] != '>')
 	{
@@ -54,7 +110,7 @@ void	cut_outfile_red(char **string)
 }
 
 /* Saves the outfile in the t_info struct. */
-void expansion_outfile_red(char **string, t_vars *ms)
+void outfile_redirection(char **string, t_vars *ms)
 {
 	int		k;
 	int		j;
@@ -69,68 +125,196 @@ void expansion_outfile_red(char **string, t_vars *ms)
 		k++;
 	tmp = ft_substr((*string), j, k - j);
 	ms->info->outfile = tmp;
+	ft_cut_outfile_redirections(string);
 }
 
-void	outfile_fd(t_vars *ms)
+static void	redirection_expansion_output(t_vars *ms, int redirection_out, char **string)
 {
-	if (ms->info->output_op == -1)
+	if (redirection_out == 0)
+		ms->info->output_op = 0;
+	if (redirection_out != -1)
 	{
-		if (ms->info->fd_out != STDOUT_FILENO)
-			close(ms->info->fd_out);
-		ms->info->fd_out = open(ms->info->outfile, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	}
-	if (ms->info->output_op == -2)
-	{
-		if (ms->info->fd_out != STDOUT_FILENO)
-			close(ms->info->fd_out);
-		ms->info->fd_out = open(ms->info->outfile, O_RDWR | O_CREAT | O_APPEND, 0644);
-	}
-	if (ms->info->fd_out == -1 || access(ms->info->outfile, W_OK) != 0)
-		perror("Error");
-}
-
-static void	output_redirection(t_vars *ms, char **string, int red_out)
-{
-	while (red_out != -1)
-	{
-		if ((*string)[red_out] == '>')
+		if ((*string)[redirection_out] == '>' && (*string)[redirection_out + 1] == '>')
 		{
-			if ((*string)[red_out + 1] == '>')
-				ms->info->output_op = -2;
-			else
-				ms->info->output_op = -1;
+			ms->info->output_op = -2;
+			outfile_redirection(string, ms);
 		}
-		expansion_outfile_red(string, ms);
-		cut_outfile_red(string);
-		outfile_fd(ms);
-		red_out = ft_strchr_pos(*string, '>');
+		else
+		{
+			ms->info->output_op = -1;
+			outfile_redirection(string, ms);
+		}
 	}
+}
+
+static void	redirection_expansion_input(t_vars *ms, int redirection_in, char **string)
+{
+	if (redirection_in == -1)
+			ms->info->input_op = 0;
+		if (redirection_in != -1)
+		{
+			if ((*string)[redirection_in] == '<' && (*string)[redirection_in + 1] == '<')
+			{
+				ms->info->input_op = -2;
+				infile_redirection(string, ms);
+			}
+			else
+			{
+				ms->info->input_op = -1;
+				infile_redirection(string, ms);
+			}
+		}
 }
 
 /* Funktion compares the position of a redirection, first '<' then '>' and tests
 if '<<' or '>>' are called. It then tests further if the pipe comes before
 the redirection. */
-void	check_redirections(char **string, t_vars *ms)
+void	check_redirections(char **string, int pipe_marker, t_vars *ms)
 {
-	int		red_in;
-	int		red_out;
+	int		redirection_in;
+	int		redirection_out;
 
-	red_in = 0;
-	red_out = 0;
-	red_in = ft_strchr_pos(*string, '<');
-	ms->info->fd_in = STDIN_FILENO;
-	if (red_in != -1)
-		input_redirection(ms, string, red_in);
-	else
-		ms->info->input_op = 0;
-	red_out = ft_strchr_pos(*string, '>');
-	ms->info->fd_out = STDOUT_FILENO;
-	if (red_out != -1)
-		output_redirection(ms, string, red_out);
-	else
-		ms->info->output_op = 0;
+	redirection_in = ft_strchr_pos((*string), '<');
+	redirection_expansion_input(ms, redirection_in, string);
+	redirection_out = ft_strchr_pos(*string, '>');
+	redirection_expansion_output(ms, redirection_out, string);
+	if (ft_strchr_pos(*string, '<') != -1 && ft_strchr_pos(*string, '>') != -1)
+	check_redirections((&*string), pipe_marker, ms);
 
+	//add here info struct to open files!!!! and save here_doc and file descriptors in info struct
 }
+
+// //heredocs werden vor allem anderen ausgeführt, nicht nur pro command sondern in der ganzen input zeile
+// //redirections for pipes
+// //die letzte redirection zählt
+
+// // '<<'
+// //3.6.6 Here Documents
+// // This type of redirection instructs the shell to read input from the current source until a line containing only word (with no trailing blanks) i
+// // s seen. All of the lines read up to that point are then used as the standard input (or file descriptor n if n is specified) for a command.
+// // The format of here-documents is:
+// // [n]<<[-]word
+// //         here-document
+// // delimiter
+// // No parameter and variable expansion, command substitution, arithmetic expansion, or filename expansion is performed on word. If any part of word is
+// // quoted, the delimiter is the result of quote removal on word, and the lines in the here-document are not expanded. If word is unquoted, all lines
+// // of the here-document are subjected to parameter expansion, command substitution, and arithmetic expansion, the character sequence \newline is ignored,
+// // and ‘\’ must be used to quote the characters ‘\’, ‘$’, and ‘`’.
+// // If the redirection operator is ‘<<-’, then all leading tab characters are stripped from input lines and the line containing delimiter. This allows
+// // here-documents within shell scripts to be indented in a natural fashion.
+
+
+
+
+// /* Cuts out the redirections associated with the outfile from the cmd_line. */
+// void	cut_outfile_red(char **string)
+// {
+// 	char	*tmp;
+// 	int		i;
+// 	int		p;
+
+// 	i = 0;
+// 	p = 0;
+// 	tmp = NULL;
+// 	while ((*string)[i] != '>')
+// 	{
+// 		tmp = ft_strnjoin(tmp, (*string)[i], 1);
+// 		i++;
+// 	}
+// 	while ((*string)[i] == '>')
+// 		i++;
+// 	while ((*string)[i] == ' ' && (*string)[i] != '\0')
+// 		i++;
+// 	i++;
+// 	while ((*string)[i] != ' ' && (*string)[i] != '\0')
+// 		i++;
+// 	while ((*string)[i] == ' ' && (*string)[i] != '\0')
+// 		i++;
+// 	while ((*string)[i] != '\0')
+// 		tmp = ft_strnjoin(tmp, (*string)[i++], 1);
+// 	free(*string);
+// 	*string = NULL;
+// 	*string = tmp;
+// }
+
+// /* Saves the outfile in the t_info struct. */
+// void expansion_outfile_red(char **string, t_vars *ms)
+// {
+// 	int		k;
+// 	int		j;
+// 	char	*tmp;
+
+// 	k = 0;
+// 	j = ft_strchr_pos((*string), '>');
+// 	while (((*string)[j] == '>' || (*string)[j] == ' ') && (*string)[j] != '\0')
+// 		j++;
+// 	k = j;
+// 	while ((*string)[k] != ' ' && (*string)[k] != '\0')
+// 		k++;
+// 	tmp = ft_substr((*string), j, k - j);
+// 	ms->info->outfile = tmp;
+// }
+
+// void	outfile_fd(t_vars *ms)
+// {
+// 	if (ms->info->output_op == -1)
+// 	{
+// 		if (ms->info->fd_out != STDOUT_FILENO)
+// 			close(ms->info->fd_out);
+// 		ms->info->fd_out = open(ms->info->outfile, O_RDWR | O_CREAT | O_TRUNC, 0644);
+// 	}
+// 	if (ms->info->output_op == -2)
+// 	{
+// 		if (ms->info->fd_out != STDOUT_FILENO)
+// 			close(ms->info->fd_out);
+// 		ms->info->fd_out = open(ms->info->outfile, O_RDWR | O_CREAT | O_APPEND, 0644);
+// 	}
+// 	if (ms->info->fd_out == -1 || access(ms->info->outfile, W_OK) != 0)
+// 		perror("Error");
+// }
+
+// static void	output_redirection(t_vars *ms, char **string, int red_out)
+// {
+// 	while (red_out != -1)
+// 	{
+// 		if ((*string)[red_out] == '>')
+// 		{
+// 			if ((*string)[red_out + 1] == '>')
+// 				ms->info->output_op = -2;
+// 			else
+// 				ms->info->output_op = -1;
+// 		}
+// 		expansion_outfile_red(string, ms);
+// 		cut_outfile_red(string);
+// 		outfile_fd(ms);
+// 		red_out = ft_strchr_pos(*string, '>');
+// 	}
+// }
+
+// /* Funktion compares the position of a redirection, first '<' then '>' and tests
+// if '<<' or '>>' are called. It then tests further if the pipe comes before
+// the redirection. */
+// void	check_redirections(char **string, t_vars *ms)
+// {
+// 	int		red_in;
+// 	int		red_out;
+
+// 	red_in = 0;
+// 	red_out = 0;
+// 	red_in = ft_strchr_pos(*string, '<');
+// 	ms->info->fd_in = STDIN_FILENO;
+// 	if (red_in != -1)
+// 		input_redirection(ms, string, red_in);
+// 	else
+// 		ms->info->input_op = 0;
+// 	red_out = ft_strchr_pos(*string, '>');
+// 	ms->info->fd_out = STDOUT_FILENO;
+// 	if (red_out != -1)
+// 		output_redirection(ms, string, red_out);
+// 	else
+// 		ms->info->output_op = 0;
+
+// }
 
 
 /* Cuts out the redirections associated with the infile from the cmd_line. */
--- src/start_main.c
+++ src/start_main.c
@@ -8,8 +8,7 @@ static char	**copy_strarray(char **strarray)
 	i = 0;
 	while (strarray[i] != NULL)
 		i++;
-	// printf("envp: %d\n", i);
-	ret = (char **)malloc(i * sizeof(char *));
+	ret = (char **)malloc((i + 1) * sizeof(char *));
 	if (ret == NULL)
 		return (NULL);
 	i = 0;
@@ -19,21 +18,22 @@ static char	**copy_strarray(char **strarray)
 		i++;
 	}
 	ret[i] = NULL;
-	// printf("%s\n", strarray[1]);
-	// printf("%s\n", ret[1]);
 	return (ret);
 }
 
 /* Initialises main struct ms as well as builtin and env. */
 int	init_struct(t_vars *ms, char **envp)
 {
+	ms->env = NULL;											// ditto: warum muss das NULL gesetzt werden?
+	// ms->env = malloc(sizeof(t_env));						// Hier gibt's Fehler, wenn das einkommentiert wird. 
 	if (init_env(ms, envp) != 0)
 		return (1);
 	init_builtin(ms);
-	ms->cmd = ft_calloc(1, sizeof(t_cmd));
-	ms->cmd = NULL;
+	// ms->cmd = ft_calloc(1, sizeof(t_cmd));				// Kathi: war auskommentiert? Mio: Brauchen wir das wirklich?! Scheint auch ohne zu gehen. Warum?
+	ms->cmd = NULL;											// Warum muessen wir alles auf NULL setzen? Dachte, das machen wir in der main mit ms = (t_vars){0};? Scheint aber wichtig zu sein, weil sonst komische Fehler.
 	ms->envp = copy_strarray(envp);
-	ms->info = ft_calloc(1, sizeof(t_info));
+	ms->info = ft_calloc(1, sizeof(t_info));				// Warum brauchen wir das hier? Gibt sonst segfault. Ich muss dieses ganze mem-alloc Thema unbedingt noch verstehen.
+	// ms->info = NULL;
 	ms->exit_status = 0;
 	ms->tmp_fd = dup(STDIN_FILENO);
 	ft_memset(ms->info, 0, sizeof(t_info));
@@ -66,9 +66,7 @@ int	read_line(t_vars *ms)
 int	main(int argc, char **argv, char **envp)
 {
 	t_vars	ms;
-	int		i;
 
-	i = 0;
 	if (argc < 1 || argv[1])
 		return (-1);
 	ms = (t_vars){0};
@@ -92,3 +90,96 @@ int	main(int argc, char **argv, char **envp)
 	}
 	return (0);
 }
+
+// static char	**copy_strarray(char **strarray)
+// {
+// 	char	**ret;
+// 	int		i;
+
+// 	i = 0;
+// 	while (strarray[i] != NULL)
+// 		i++;
+// 	// printf("envp: %d\n", i);
+// 	ret = (char **)malloc(i * sizeof(char *));
+// 	if (ret == NULL)
+// 		return (NULL);
+// 	i = 0;
+// 	while (strarray[i] != NULL)
+// 	{
+// 		ret[i] = ft_strdup(strarray[i]);
+// 		i++;
+// 	}
+// 	ret[i] = NULL;
+// 	// printf("%s\n", strarray[1]);
+// 	// printf("%s\n", ret[1]);
+// 	return (ret);
+// }
+
+// /* Initialises main struct ms as well as builtin and env. */
+// int	init_struct(t_vars *ms, char **envp)
+// {
+// 	if (init_env(ms, envp) != 0)
+// 		return (1);
+// 	init_builtin(ms);
+// 	ms->cmd = ft_calloc(1, sizeof(t_cmd));
+// 	ms->cmd = NULL;
+// 	ms->envp = copy_strarray(envp);
+// 	ms->info = ft_calloc(1, sizeof(t_info));
+// 	ms->exit_status = 0;
+// 	ms->tmp_fd = dup(STDIN_FILENO);
+// 	ft_memset(ms->info, 0, sizeof(t_info));
+// 	return (0);
+// }
+
+// /* Reads user input and saves readline() in ms struct. */
+// int	read_line(t_vars *ms)
+// {
+// 	char	*prompt;
+
+// 	prompt = NULL;
+// 	prompt = create_prompt(ms);
+// 	if (ms->cmd_line)
+// 		ft_free_string(ms->cmd_line);
+// 	rl_init();
+// 	if (prompt)
+// 		ms->cmd_line = readline(prompt);
+// 	else
+// 		ms->cmd_line = readline("minishell ॐ  ");
+// 	rl_reset();
+// 	if (ms->cmd_line == NULL)							// (x) Makes CTRL+D work.
+// 		free_and_exit(ms, 1, 0);
+// 	if (ms->cmd_line && *ms->cmd_line)
+// 		add_history (ms->cmd_line);
+// 	free(prompt);
+// 	return (0);
+// }
+
+// int	main(int argc, char **argv, char **envp)
+// {
+// 	t_vars	ms;
+// 	int		i;
+
+// 	i = 0;
+// 	if (argc < 1 || argv[1])
+// 		return (-1);
+// 	ms = (t_vars){0};
+// 	init_struct(&ms, envp);
+// 	while (1)
+// 	{
+// 		read_line(&ms);
+// 		create_cmd_table(&ms);
+// 		get_paths(&ms);
+// 		pipex(&ms);
+// 		// print_lst(&ms);
+// 		// print_lst_last(&ms);
+// 		free_cmd_struct(&ms);
+// 		if (ms.line)
+// 		{
+// 			free(ms.line);
+// 			ms.line = NULL;
+// 		}
+// 		reset_info_struct(ms.info);
+// 		// system("leaks minishell");
+// 	}
+// 	return (0);
+// }
