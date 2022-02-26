c675254a4ef2a6d715ac3e3fb1b563fd901b991a
khammers <khammers@student.42heilbronn.de>
2022-02-26 18:41:19 +0100

    fixing multiple infile outfile redirections, cutting empty quotes

--- src/cmd_redirection_output.c
+++ src/cmd_redirection_output.c
@@ -41,15 +41,17 @@ void	expansion_outfile_red(char **string, t_vars *ms, int j)
 	while ((*string)[k] != ' ' && (*string)[k] != '\0')
 		k++;
 	tmp = ft_substr((*string), j, k - j);
+	// if (ms->info.outfile != NULL)
+	ft_free_string(&ms->info.outfile);
 	ms->info.outfile = tmp;
 }
 
 void	outfile_fd(t_vars *ms)
 {
+	if (ms->info.fd_out != STDOUT_FILENO)
+		close(ms->info.fd_out);
 	if (ms->info.output_op == -1)
 	{
-		if (ms->info.fd_out != STDOUT_FILENO)
-			close(ms->info.fd_out);
 		ms->info.fd_out = open(ms->info.outfile,
 				O_RDWR | O_CREAT | O_TRUNC, 0644);
 	}
@@ -61,7 +63,10 @@ void	outfile_fd(t_vars *ms)
 				O_RDWR | O_CREAT | O_APPEND, 0644);
 	}
 	if (ms->info.fd_out == -1 || access(ms->info.outfile, W_OK) != 0)
+	{
 		perror("Error");
+		ft_free_string(&ms->info.outfile);
+	}
 }
 
 void	output_redirection(t_vars *ms, char **string, int red_out)
@@ -83,6 +88,6 @@ void	output_redirection(t_vars *ms, char **string, int red_out)
 		expansion_outfile_red(string, ms, red_out);
 		cut_outfile_red(string, red_out);
 		outfile_fd(ms);
-		red_out = ft_strchr_pos(string[red_out + 1], '>');
+		red_out = ft_strchr_pos(&(*string)[red_out], '>');
 	}
 }
