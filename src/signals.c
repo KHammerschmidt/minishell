95283ddb2c835ce32ab054f7be3b5bba5c778647
Mio <m.ortmeier@outlook.com>
2022-03-07 10:21:37 +0100

    added comments in signals.c

--- src/signals.c
+++ src/signals.c
@@ -1,7 +1,7 @@
 #include "../header/minishell.h"
 
-/* Intercepts SIGINT (CTRL+C) and prevents minishell from quitting, prints */
-/* empty new line instead. Does nothing for SIGQUIT (CTRL+\).              */
+/* Intercepts SIGINT (CTRL+C) during readline call and prevents	*/
+/* minishell from quitting, prints empty new line instead.		*/
 void	signal_handler_0(int signum)
 {
 	(void)signum;
@@ -11,17 +11,17 @@ void	signal_handler_0(int signum)
 	rl_redisplay();
 }
 
+/* Intercepts SIGINT (CTRL+C) during heredoc readline call.	*/
+/* Exits heredoc and returns to minishell.					*/
 void	signal_handler_1(int signum)
 {
 	(void)signum;
 	close(STDIN_FILENO);
 	write(1, "\n", 1);
 	rl_replace_line("", 0);
-	// rl_on_new_line();
-	// rl_redisplay();
 }
 
-/* Initialises signal handling and terminal output for readline(). */
+/* Initiates signal handling and terminal output for readline(). */
 void	rl_init(int i)
 {
 	struct termios	termios_p;
